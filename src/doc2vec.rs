27910709dfc396bee56f80c8df14025c146d274d
Michael May <mskmay66@gmail.com>
2025-07-25 16:52:50 -0400

    (feat) finished backward passes for w2v and d2v

--- src/doc2vec.rs
+++ src/doc2vec.rs
@@ -1,13 +1,13 @@
 mod word2vec;
 
-use word2vec::{Layer, sigmoid, binary_entropy};
+use word2vec::{Layer, sigmoid, binary_entropy_loss};
 use pyo3::prelude::*;
 use ndarray::{Array2, ArrayView2};
 use ndarray_rand::rand_distr::Uniform;
 use rayon::prelude::*;
 
 #[pyclass]
-struct Doc2Vec {
+struct _Doc2Vec {
     embedding_dim: usize,
     vocab_size: usize,
     input_layer: Layer,
@@ -17,13 +17,13 @@ struct Doc2Vec {
 }
 
 #[pymethods]
-impl Doc2Vec {
+impl _Doc2Vec {
     #[new]
     fn new(embedding_dim: usize, vocab_size: usize, lr: f32) -> Self {
         let input_layer = Layer::new(embedding_dim);
         let target_layer = Layer::new(embedding_dim);
         let doc_layer = Layer::new(embedding_dim);
-        Doc2Vec {
+        _Doc2Vec {
             embedding_dim,
             vocab_size,
             input_layer,
@@ -42,23 +42,20 @@ impl Doc2Vec {
     }
 
 
-    fn backward(&self, output: Array2<f32>, label: ArrayView2<u32>) {
-        // TODO: Fix this
-        let loss = binary_entropy(label, output);
-        let input_grad = loss.dot(&self.target_layer.weights.t());
-        let target_grad = loss.dot(&self.input_layer.weights.t());
-        let doc_grad = loss.dot(&self.doc_layer.weights.t());
-
-        // get gradients with respect to biases
-        let input_bias_grad = loss.sum_axis(Axis(0));
-        let target_bias_grad = loss.sum_axis(Axis(0));
-        let doc_bias_grad = loss.sum_axis(Axis(0));
+    fn backward(&self, loss: Array2<f32>, input: Array2<f32>, target: Array2<f32>, output: Array2<f32>, y0: Array2<f32>, y1: Array2<f32>, y3: Array2<f32>) {
+        // let base = loss.dot()
+        let wr_z = loss.dot(output * (1 - output)); // sigmoid gradient
+        let input_doc_avg_bias_grad = wr_z.dot(y1); // gradient w.r.t. input word embedding
+        let target_bias_grad = wr_z.dot((y0 + y3) / 2); // gradient w.r.t. target word embedding
+        let target_grad = target_bias_grad.dot(target.t());
+        let input_grad = input_doc_avg_bias_grad.dot(input.t()) * 0.5; // gradient w.r.t. input layer weights
+        let doc_grad = input_doc_avg_bias_grad.dot(doc.t()) * 0.5; // gradient w.r.t. doc layer weights
 
         // update weights and biases
-        self.input_layer.biases -= &(input_bias_grad * self.lr);
+        self.input_layer.biases -= &(input_doc_avg_bias_grad * self.lr * 0.5);
+        self.doc_layer.biases -= &(input_doc_avg_bias_grad * self.lr * 0.5);
         self.target_layer.biases -= &(target_bias_grad * self.lr);
-        self.doc_layer.biases -= &(doc_bias_grad * self.lr);
-
+        self.doc_layer.biases -= &(doc_grad * self.lr);
         self.input_layer.weights -= &(input_grad * self.lr);
         self.target_layer.weights -= &(target_grad * self.lr);
         self.doc_layer.weights -= &(doc_grad * self.lr);
--- src/word2vec.rs
+++ src/word2vec.rs
@@ -4,8 +4,8 @@ use pyo3::prelude::*;
 use rayon::prelude::*;
 
 
-fn binary_entropy(target: Array2<u32>, pred: Array2<f32>)-> f32 {
-    - (target * pred.ln()) + (1 - target) * (1 - pred).ln()
+fn binary_entropy_loss(target: Array2<u32>, pred: Array2<f32>)-> f32 {
+    (pred - target) / (pred * (1 - target))
 }
 
 fn sigmoid(output: Array2<f32>) -> Array2<f32> {
@@ -14,14 +14,19 @@ fn sigmoid(output: Array2<f32>) -> Array2<f32> {
 }
 
 pub struct Layer {
-    weights: Array2<f32>,
-    biases: Array2<f32>,
+    pub weights: Array2<f32>,
+    pub biases: Array2<f32>,
 }
 
 impl Layer {
+    fn xavier_uniform(shape: (usize, usize)) -> Array2<f32> {
+        let limit = (6.0 / (shape.0 + shape.1) as f32).sqrt();
+        Array2::random(shape, Uniform::new(-limit, limit))
+    }
+
     fn new(embedding_dim: usize) -> Self {
-        let weights = Array2::random((1, embedding_dim), Uniform::new(-0.1, 0.1));
-        let biases = Array2::random((1, embedding_dim), Uniform::new(-0.1, 0.1));
+        let weights = xavier_uniform(1, embedding_dim);
+        let biases = xavier_uniform(1, embedding_dim);
         Layer { weights, biases }
     }
 
@@ -32,53 +37,56 @@ impl Layer {
 
 
 #[pyclass]
-pub struct Word2Vec {
+pub struct _Word2Vec {
     #[pyo3(get)]
     pub embedding_dim: usize,
     #[pyo3(get)]
     pub vocab_size: usize,
-    pub input_layer: Layer,
-    pub target_layer: Layer,
     #[pyo3(get)]
     pub lr: f32,
+    pub input_layer: Layer,
+    pub target_layer: Layer
 }
 
 #[pymethods]
-impl Word2Vec {
+impl _Word2Vec {
     #[new]
     fn new(embedding_dim: usize, lr: f32) -> Self {
         let input_layer = Layer::new(embedding_dim);
         let target_layer = Layer::new(embedding_dim);
-        Word2Vec {
+        _Word2Vec {
             embedding_dim,
             vocab_size,
+            lr,
             input_layer,
-            target_layer,
-            lr
+            target_layer
         }
     }
 
 
-    fn forward(&self, input: ArrayView2<usize>, target: ArrayView2<usize>) -> Array2<f32> {
+    fn forward(&self, input: ArrayView2<usize>, target: ArrayView2<usize>) -> (Array2<f32>, Array2<f32>, Array2<f32>) {
         let input_output = self.input_layer.forward(input);
         let target_ouput = self.target_layer.forward(target);
-        sigmoid(input_output.dot(&target_ouput.t()).diag());
+        let consine_sim = (input_output * target_output).par_iter().sum::<f32>();
+        (sigmoid(consine_sim) , input_output, target_output)
     }
 
-    fn backward(&self, output: Array2<f32>, label: ArrayView2<u32>) {
-        // TODO: Fix this
-        let loss = binary_entropy(label, output);
-        let input_grad = loss.dot(&self.target_layer.weights.t());
-        let target_grad = loss.dot(&self.input_layer.weights.t());
-
-        // get gradients with respect to biases
-        let input_bias_grad = loss.sum_axis(Axis(0));
-        let target_bias_grad = loss.sum_axis(Axis(0));
+    fn backward(&self, loss: Array2<f32>, input: Array2<f32>, target: Array2<f32>, output: Array2<f32>, y0: Array2<f32>, y1: Array2<f32>) {
+        // loss is the binary cross-entropy loss
+        // input is the word vector for the input word
+        // target is the context word vector
+        // output is the sigmoid output of the dot product of input and target
+        // y0 is the input word embedding
+        // y1 is the target word embedding
+        let wr_z = loss.dot(output * (1 - output)); // sigmoid gradient
+        let input_bias_grad = wr_z.dot(y1); // gradient w.r.t. input word embedding
+        let target_bias_grad = wr_z.dot(y0); // gradient w.r.t. target word embedding
+        let input_grad = input_bias_grad.dot(input.t()); // gradient w.r.t. input layer weights
+        let target_grad = target_bias_grad.dot(target.t()); // gradient w.r.t. target layer weights
 
         // update weights and biases
         self.input_layer.biases -= &(input_bias_grad * self.lr);
         self.target_layer.biases -= &(target_bias_grad * self.lr);
-
         self.input_layer.weights -= &(input_grad * self.lr);
         self.target_layer.weights -= &(target_grad * self.lr);
     }
@@ -88,10 +96,11 @@ impl Word2Vec {
         let target_array = Array2::from_shape_vec((1, target.len()), target).unwrap();
         let label_array = Array2::from_shape_vec((1, label.len()), label).unwrap();
 
-        let output = self.forward(input_array.view(), target_array.view());
+        let (output, y0, y1) = self.forward(input_array.view(), target_array.view());
 
         if grad.unwrap_or(false) {
-            self.backward(output, label_array.view());
+            let loss = binary_entropy_loss(label_array.view(), output.view());
+            self.backward(loss, input_array, target_array, output, y0, y1);
         }
 
         Ok(output.par_iter().map(|&x| x as f32).collect())
