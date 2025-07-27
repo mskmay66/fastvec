084b013665716303a43effe00174c3b064ddcf5b
Michael May <mskmay66@gmail.com>
2025-07-27 13:17:33 -0400

    (tests) doc layer unit tests

--- src/doc2vec.rs
+++ src/doc2vec.rs
@@ -29,3 +29,39 @@ impl DocumentLayer {
         self.layer.biases -= &(doc_gradient * self.lr);
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use::super::*;
+    use word2vec::binary_entropy_loss;
+
+    #[test]
+    fn test_doc_layer_creation() {
+        let doc_layer = DocumentLayer::new(3, 0.01);
+        assert_eq!(doc_layer.layer.weights.shape(), (3, 3));
+        assert_eq!(doc_layer.layer.biases.shape(), (3,));
+        assert_eq!(doc_layer.lr, 0.01);
+    }
+
+    #[test]
+    fn test_doc_layer_forward() {
+        let doc_layer = DocumentLayer::new(3, 0.01);
+        let input = Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap();
+        let word_embedding = Array2::from_shape_vec((2, 3), vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0]).unwrap();
+        let (output, doc_vec) = doc_layer.forward(input.view(), word_embedding.view());
+        assert_eq!(output.shape(), (2,));
+        assert_eq!(doc_vec.shape(), (2, 3));
+    }
+
+    #[test]
+    fn test_doc_layer_backward() {
+        let doc_layer = DocumentLayer::new(3, 0.01);
+        let input = Array2::from_shape_vec((2, 3), vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap();
+        let word_embedding = Array2::from_shape_vec((2, 3), vec![1.0, 1.0, 1.0, 1.0, 1.0, 1.0]).unwrap();
+        let (output, doc_vec) = doc_layer.forward(input.view(), word_embedding.view());
+        let loss = binary_entropy_loss(Array2::ones((2, 1)), output.clone());
+        doc_layer.backward(loss, doc_vec.view(), input.view());
+        assert!(doc_layer.layer.weights.iter().all(|&x| x != 0.0));
+    }
+}
